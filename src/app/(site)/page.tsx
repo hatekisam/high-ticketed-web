c5a717715c6388a5de45a318957f35e1ee25e37d
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-30 09:27:34 +0200

    added all home images

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,9 +1,11 @@
 import Landing from "@/components/home/Landing";
+import SliderSection from "@/components/home/Slider";
 
 export default function Home() {
   return (
     <div className="bg-[#121316] pt-20 text-white">
       <Landing />
+      <SliderSection />
     </div>
   );
 }
