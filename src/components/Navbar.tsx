4cc4994e90bc90c567c5a8ea4b5971f81c34614a
Mugisha Yves <mugishayves189000@gmail.com>
2023-12-30 09:04:27 +0200

    made the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,39 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar: React.FC = () => {
-  return <div className="rounded-md border w-full h-[50px]">Navbar</div>;
+  const routes = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About Us",
+      href: "/about",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+    {
+      title: "Support",
+      href: "/support",
+    },
+  ];
+  return (
+    <div className="text-white fixed left-0 top-0 bg-[#121316] w-full flex justify-between p-4">
+      <p className="text-2xl">HTicketed Web</p>
+      <div>
+        {routes.map((route: any, index: number) => {
+          return (
+            <Link href={route.href} className="text-sm font-bold px-5">
+              {route.title}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
